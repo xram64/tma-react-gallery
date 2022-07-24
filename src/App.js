1a988c4a55acd41d7c2488fa79c607bba4df2094
xram64 <33797815+xram64@users.noreply.github.com>
2022-07-23 22:43:49 -0400

    Rough component layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,135 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import logo from './logo.svg';
 
+/* [App]
+• Description
+  - Header with BG image and links to switch between photos/videos
+  - Full-window photo/video view (thumbnail row along the bottom? pre-generate thumbnails for each picture?)
+
+• Page layout components
+  - Header
+  - Content
+  - Details
+    · Get date/time info from filename
+
+• Other components
+  - MenuButton
+*/
+
+class MenuButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pressed: false
+    };
+  }
+
+  render() {
+    return (
+      <button
+        className={this.state.pressed ? 'pressed' : ''}
+        onClick={() => {this.setState({pressed: true}); this.props.onClick(); }}
+      >
+        {this.props.label}
+      </button>
+    );
+  }
+}
+
+class Header extends React.Component {
+
+  //   // This binding is necessary to make `this` work in the callback
+  //   this.handleClick = this.handleClick.bind(this);
+
+  handleClick(target) {
+    if (target === 'photos') {
+      // Switch to Images page
+    }
+    if (target === 'videos') {
+      // Switch to Videos page
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <MenuButton label="Photos" onClick={() => this.handleClick('photos')} />
+        <MenuButton label="Videos" onClick={() => this.handleClick('videos')} />
+      </div>
+    );
+  }
+}
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mode: 'photos',
+      currentPhoto: null,
+      currentVideo: null,
+    };
+  }
+
+  componentWillUnmount() {
+    // Clean up
+    //this.????.dispose();
+  }
+
+  render() {
+    if (this.state.mode === 'photos') {
+      return (
+        <div className="content" id="c_photo">
+          <img src={this.state.currentPhoto} />
+        </div>
+      );
+    }
+    else {
+      return (
+        <div className="content" id="c_video">
+          <video src={this.state.currentVideo} />
+        </div>
+      );
+    }
+  }
+}
+
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    return (
+      <div className="details">
+        
+      </div>
+    );
+  }
+
+}
+
+
+// Main app layout
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Header /> */}
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="App-content">
+        <Content />
+      </div>
+      
+      <div className="App-details">
+        <Details />
+      </div>
+
     </div>
   );
 }
