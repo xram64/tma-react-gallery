57e3f0ca1f989c6585ebe834a17b67c6037bcabb
xram64 <33797815+xram64@users.noreply.github.com>
2022-08-07 01:45:51 -0400

    Navigation and styling pass
    - Set URL fragments to permalink the active image/video.
    - Added more functionality and set styling for Navigation buttons.
    - Made a responsively-sized set of number selectors for Navigation.
    - Added keyboard shortcuts through a new event listener custom hook.
    - Fixed video controls/playback.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-// React
-import React, { useState, useEffect } from 'react';
+// React //
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import './App.css';
 
-// AWS
+// AWS //
 const s3_bucketParams = { name: "tma-meetup-kushoglake-2022", region: "us-east-1" };
 
 /*  ——————————————————————————————————————————————————————————————————————————————————————————————————————————————
@@ -11,10 +11,11 @@ const s3_bucketParams = { name: "tma-meetup-kushoglake-2022", region: "us-east-1
  *    - Full-window image/video view
  *
  *  • TODO
- *    (!) Fix video playback/controls.
- *    (!) Format nav buttons. Add numbered nav buttons. Add 'First'/'Last' nav buttons.
  *    (•) Add (pop-out?) list view for images/videos in bucket.
  *    (•) Add sorting options/controls.
+ *    (•) Add an info icon to show tooltip in mouseover (keyboard shortcuts, etc).
+ *    (?) Add menu option to open image/video in new tab.
+ *    (?) Add menu option to download image/video.
  *    (?) Add a thumbnail row along the bottom (pre-generate thumbnails for each picture).
  *    (?) Set cookie to remember last image/video position.
  * 
@@ -22,26 +23,51 @@ const s3_bucketParams = { name: "tma-meetup-kushoglake-2022", region: "us-east-1
 */
 
 
-//|  ——————————————————————  |\\
-//|  |  Helper functions  |  |\\
-//|  ——————————————————————  |\\
+////|  ——————————————————————  |\\\\
+////|  |  Helper functions  |  |\\\\
+////|  ——————————————————————  |\\\\
 
 // A proper modulus function
 function mod(n, m) {
   return ((n % m) + m) % m;
 }
 
-// Returns an incremented 'index', unless the new index exceeds a 'max' value
+// Returns an incremented 'index', unless the new index exceeds a 'max' value.
 function incrementWithClamp(index, max) {
   var newIndex = index + 1;
   if (newIndex > max) newIndex = max;
   return newIndex;
 }
-// Returns an decremented 'index', unless the new index exceeds a 'min' value (default 0)
+// Returns an decremented 'index', unless the new index exceeds a 'min' value (default 0).
 function decrementWithClamp(index, min = 0) {
   var newIndex = index - 1;
   if (newIndex < min) newIndex = min;
   return newIndex;
+}
+
+// Returns a sequence of integers of a given 'length' around a 'center' index.
+// Odd 'length' sequences will have an equal number of indices on both sides of the 'center'.
+// Even 'length' sequences will have one extra number to the right of the 'center'.
+function centeredIndexSequence(center, length, minIndex = 0, maxIndex = Number.POSITIVE_INFINITY) {
+  var seq = [];
+  var indexRange = maxIndex - minIndex + 1;
+  var start = center - Math.floor((length - 1) / 2);
+  var end = start + length - 1;
+
+  // If the provided 'length' is larger than the 'indexRange', the full [minIndex, maxIndex] index list will be returned.
+  if (length > indexRange) {
+    for (var i = minIndex; i <= maxIndex; i++) { seq.push(i); }
+    return seq;
+  }
+
+  // Generate sequence (may include out-of-bounds indices)
+  for (var i = start; i <= end; i++) { seq.push(i); }
+
+  // If the centered sequence would cross the [minIndex, maxIndex] boundary, it will be shifted to fit.
+  if (seq[0] < minIndex) { seq = seq.map((idx) => idx + (minIndex - seq[0])) }
+  else if (seq[seq.length - 1] > maxIndex) { seq = seq.map((idx) => idx - (seq[seq.length - 1] - maxIndex)) }
+
+  return seq;
 }
 
 // Returns the Unix timestamp from a 'YYYYMMDD_HHMMSS' formatted filename
@@ -56,7 +82,7 @@ function getUnixTimestamp(filename) {
 }
 
 // Reads an XML NodeList of <Contents> elements from an S3 bucket and returns a
-//   list of {mediatype, foldername, filename, src} objects, covering all files in the bucket.
+//  list of {mediatype, foldername, filename, src} objects, covering all files in the bucket.
 function parseBucketFileList(contentNodes, bucketName) {
   var contentList = [];
 
@@ -135,9 +161,9 @@ function parseDetails(filename, foldername) {
 }
 
 
-//|  ——————————————————————  |\\
-//|  |     Components     |  |\\
-//|  ——————————————————————  |\\
+////|  ——————————————————————  |\\\\
+////|  |     Components     |  |\\\\
+////|  ——————————————————————  |\\\\
 
 function Content(props) {
   // [Update triggers]
@@ -164,7 +190,7 @@ function Content(props) {
   const s3_objects = useGetS3Objects(s3_bucketParams.name);  // Custom hook: Get list of S3 objects from bucket
 
 
-  //|  Initialization  |\\
+  /// [Content — Initialization] ///
 
   // Get image/video lists from S3 bucket
   useEffect(() => {
@@ -177,27 +203,117 @@ function Content(props) {
 
   // Initialize defaults for each media type
   useEffect(() => {
-    if (imagesList.length > 0 && videosList.length > 0) {  // if the file list has been loaded
-      // HACK: Assumes initial mode is 'images'
-      var defaultImage = imagesList[0];  // default to first item in sorted list
-      setCurrentMedia({ src: defaultImage.src, filename: defaultImage.filename, foldername: defaultImage.foldername });
-      setCurrentIndex({ image: 0, video: 0 });
+    if (imagesList.length > 0 && videosList.length > 0) {  // make sure file list has been loaded
+      // Default to 'images' mode showing the first image, unless a fragment is provided in the URL (format: 'images-0' or 'videos-0')
+      if (window.location.hash.includes('#') && window.location.hash.length > 1) {
+        try {
+          let h_mode = window.location.hash.split('#')[1].split('-')[0];
+          let h_index = window.location.hash.split('#')[1].split('-')[1];
+          h_index = parseInt(h_index) - 1;  // the fragment index is 1-indexed, so shift to get the array index
+
+          // Check for valid mode and index, fail if invalid
+          if (h_mode !== 'images' && h_mode !== 'videos') throw new Error("Invalid mode in URL fragment.");
+          if (Number.isNaN(Number.parseInt(h_index))) throw new Error("Invalid index in URL fragment.");
+          if (h_mode === 'images' && (h_index < 0 || h_index >= imagesList.length)) throw new Error("Out-of-bounds index in URL fragment.");
+          if (h_mode === 'videos' && (h_index < 0 || h_index >= videosList.length)) throw new Error("Out-of-bounds index in URL fragment.");
+
+          // For a valid mode and index, set the defaults accordingly
+          let h_media = (h_mode === 'images' ? imagesList[h_index] : videosList[h_index]);
+          setCurrentMedia({ src: h_media.src, filename: h_media.filename, foldername: h_media.foldername });
+          if (h_mode === 'images') {
+            setMode('images');
+            setCurrentIndex({ image: h_index, video: 0 });
+          }
+          if (h_mode === 'videos') {
+            setMode('videos');
+            setCurrentIndex({ image: 0, video: h_index });
+          }
+        }
+        catch (err) {
+          console.log("[ERROR] " + err);
+          // Fallback to defaults if URL fragment is malformed  // DRY
+          setCurrentMedia({ src: imagesList[0].src, filename: imagesList[0].filename, foldername: imagesList[0].foldername });
+          setCurrentIndex({ image: 0, video: 0 });
+          // Clear the URL fragment (leaves the #)
+          window.location.hash = '';
+        }
+      }
+      else {
+        // Defaults (if no URL fragment is provided)  // DRY
+        setCurrentMedia({ src: imagesList[0].src, filename: imagesList[0].filename, foldername: imagesList[0].foldername });
+        setCurrentIndex({ image: 0, video: 0 });
+      }
     }
   }, [imagesList, videosList]);  // Only run when 'imagesList' or 'videoList' changes
 
   // Set 'ready' flag once all state variables have been initialized.
   useEffect(() => {
-    if (!ready && imagesList.length > 0 && videosList.length > 0 && currentMedia.src !== null && currentIndex.image > -1) {
+    if (!ready && imagesList.length > 0 && videosList.length > 0 && currentMedia.src !== null && currentIndex.image > -1 && currentIndex.video > -1) {
       setReady(true);
     }
   });
 
+  // Arrow key event handler, utilizing useCallback so the reference never changes.
+  const navKeyHandler = useCallback(
+    (event) => {
+      if (ready) {
+        let n_index = (mode === 'images') ? currentIndex.image : currentIndex.video;
+        let n_maxindex = (mode === 'images') ? imagesList.length - 1 : videosList.length - 1;
 
-  //|  Rendering  |\\
+        // Check for navigation keys
+        switch (event.key) {
+          // Back 1
+          case "ArrowLeft":
+          case "ArrowUp":
+            n_index -= 1;
+            if (n_index < 0) { n_index = 0; }
+            break;
+          // Forward 1
+          case "ArrowRight":
+          case "ArrowDown":
+            n_index += 1;
+            if (n_index > n_maxindex) { n_index = n_maxindex; }
+            break;
+          // Back 10
+          case "PageUp":
+            n_index -= 10;
+            if (n_index < 0) { n_index = 0; }
+            break;
+          // Forward 10
+          case "PageDown":
+            n_index += 10;
+            if (n_index > n_maxindex) { n_index = n_maxindex; }
+            break;
+          // Back to start
+          case "Home":
+            n_index = 0;
+            break;
+          // Forward to end
+          case "End":
+            n_index = n_maxindex;
+            break;
+          // Toggle mode
+          case "m":
+            setMode((mode === 'images') ? 'videos' : 'images');
+            break;
+        }
+
+        // Update media
+        if (mode === 'images')
+          setCurrentIndex({ image: n_index, video: currentIndex.video });
+        else if (mode === 'videos')
+          setCurrentIndex({ image: currentIndex.image, video: n_index });
+      } 
+    }, [currentIndex, setCurrentIndex, mode, ready]
+  );
+  // Add event listener for the arrow key event handler, using our custom hook
+  useEventListener("keydown", navKeyHandler);
+
+
+  /// [Content — Rendering] ///
 
   // Update on changes to 'currentIndex' or 'mode'
   useEffect(() => {
-
     if (ready) {
       console.log("[DEBUG] currentMedia.src: " + currentMedia.src + " | currentIndex.image: " + currentIndex.image + " | currentIndex.video: " + currentIndex.video);  // TEST
 
@@ -216,10 +332,8 @@ function Content(props) {
           foldername: videosList[currentIndex.video].foldername
         });
       }
-
     }
   }, [currentIndex, mode]);
-
 
 
   // If the list is ready
@@ -271,14 +385,14 @@ function Header(props) {
     <div className="header">
 
       <div className="header-menu">
-        <MenuButton label="Photos" onClick={() => props.setMode('images')} />
-        <MenuButton label="Videos" onClick={() => props.setMode('videos')} />
+        <MenuButton label="Photos" cls={(props.mode === 'images') ? "active" : ""} onClick={() => props.setMode('images')} />
+        <MenuButton label="Videos" cls={(props.mode === 'videos') ? "active" : ""} onClick={() => props.setMode('videos')} />
       </div>
 
       <Navigation
         mode={props.mode}
         imagesListLength={props.imagesListLength}
-        videosListLength={props.imagesListLength}
+        videosListLength={props.videosListLength}
         currentIndex={props.currentIndex}
         setCurrentIndex={props.setCurrentIndex}
       />
@@ -291,40 +405,101 @@ function Header(props) {
 
 function MenuButton(props) {
   return (
-    <a className="header-menu-btn" name={"menubtn" + props.label} onClick={() => { props.onClick(); }}>
+    <a className={"header-menu-btn" + " " + props.cls} name={"menubtn" + props.label} onClick={() => { props.onClick(); }}>
       {props.label}
     </a>
   );
 }
 
-
 function Navigation(props) {
+  const [windowLength, setWindowLength] = useState(5);
+
+  // One-shot effect to set up adjustments to window length based on viewport width.
+  // [Ref: https://blog.bitsrc.io/using-react-hooks-to-recognize-respond-to-current-viewport-size-c385009005c0]
+  useEffect(() => {
+    const onResize = () => {
+      if (window.innerWidth < 480)
+        setWindowLength(1);
+      else if (window.innerWidth < 768)
+        setWindowLength(3);
+      else
+        setWindowLength(5);
+    };
+    window.addEventListener('resize', onResize);  // set event listener for window size changes
+    return () => { window.removeEventListener('resize', onResize); }  // clean up
+  }, []);
+
+  // Change URL fragment (#) on navigation.
+  useEffect(() => {
+    if (props.currentIndex.image >= 0 && props.currentIndex.video >= 0) {
+      let h_index = (props.mode === 'images' ? props.currentIndex.image : props.currentIndex.video);
+      window.location.hash = "#" + props.mode + "-" + (Number.parseInt(h_index) + 1);
+    }
+  }, [props.currentIndex, props.mode]);
+
+
+  var center = 0, maxIndex = 0;
+  if (props.mode === 'images') {
+    center = props.currentIndex.image;
+    maxIndex = props.imagesListLength - 1;
+  }
+  else if (props.mode === 'videos') {
+    center = props.currentIndex.video;
+    maxIndex = props.videosListLength - 1;
+  }
+
   return (
     <div className="header-nav">
-      <NavButton label="Prev" onClick={() => {
-        if (props.mode === 'images') {
-          props.setCurrentIndex({ image: decrementWithClamp(props.currentIndex.image, 0), video: props.currentIndex.video });
-        }
-        else if (props.mode === 'videos') {
-          props.setCurrentIndex({ video: decrementWithClamp(props.currentIndex.video, 0), image: props.currentIndex.image });
-        }
+
+      {/*  First button  */}
+      <NavButton label="<<" desc="first" cls="first-last" onClick={() => {
+        if (props.mode === 'images')
+          props.setCurrentIndex({ image: 0, video: props.currentIndex.video });
+        else if (props.mode === 'videos')
+          props.setCurrentIndex({ video: 0, image: props.currentIndex.image });
       }} />
 
-      <NavButton label="Next" onClick={() => {
-        if (props.mode === 'images') {
-          props.setCurrentIndex({ image: incrementWithClamp(props.currentIndex.image, props.imagesListLength - 1), video: props.currentIndex.video });
-        }
-        else if (props.mode === 'videos') {
-          props.setCurrentIndex({ video: incrementWithClamp(props.currentIndex.video, props.videosListLength - 1), image: props.currentIndex.image });
-        }
+      {/*  Previous button  */}
+      <NavButton label="<" desc="previous" cls="prev-next" onClick={() => {
+        if (props.mode === 'images')
+          props.setCurrentIndex({ image: decrementWithClamp(props.currentIndex.image, 0), video: props.currentIndex.video });
+        else if (props.mode === 'videos')
+          props.setCurrentIndex({ video: decrementWithClamp(props.currentIndex.video, 0), image: props.currentIndex.image });
       }} />
+
+      {/*  Numbered buttons  */}
+      {centeredIndexSequence(center, windowLength, 0, maxIndex).map((i) =>
+        <NavButton key={i.toString()} label={(i + 1).toString()} desc={"item-" + (i + 1).toString()} cls={(i == center) ? "number active" : "number"} onClick={() => {
+          if (props.mode === 'images')
+            props.setCurrentIndex({ image: i, video: props.currentIndex.video });
+          else if (props.mode === 'videos')
+            props.setCurrentIndex({ image: props.currentIndex.image, video: i });
+        }} />
+      )}
+
+      {/*  Next button  */}
+      <NavButton label=">" desc="next" cls="prev-next" onClick={() => {
+        if (props.mode === 'images')
+          props.setCurrentIndex({ image: incrementWithClamp(props.currentIndex.image, props.imagesListLength - 1), video: props.currentIndex.video });
+        else if (props.mode === 'videos')
+          props.setCurrentIndex({ video: incrementWithClamp(props.currentIndex.video, props.videosListLength - 1), image: props.currentIndex.image });
+      }} />
+
+      {/*  Last button  */}
+      <NavButton label=">>" desc="last" cls="first-last" onClick={() => {
+        if (props.mode === 'images')
+          props.setCurrentIndex({ image: props.imagesListLength - 1, video: props.currentIndex.video });
+        else if (props.mode === 'videos')
+          props.setCurrentIndex({ video: props.videosListLength - 1, image: props.currentIndex.image });
+      }} />
+
     </div>
   );
 }
 
 function NavButton(props) {
   return (
-    <a className="header-nav-btn" name={"navbtn" + props.label} onClick={() => { props.onClick(); }}>
+    <a className={"header-nav-btn" + " " + props.cls} id={"navbtn_" + props.desc} onClick={() => { props.onClick(); }}>
       {props.label}
     </a>
   );
@@ -359,14 +534,13 @@ function Display(props) {
       data-time={props.mediaDetails.time}
       data-credit={props.mediaDetails.credit}
       alt=""
-      href={props.currentMedia.src}
       target="_blank"
     />;
   }
 
   // Setup video
   else if (props.mode === 'videos') {
-    mediaTag = <video
+    mediaTag = <video controls preload="metadata"
       src={props.currentMedia.src}
       className="display-media"
       id="md_video"
@@ -381,7 +555,7 @@ function Display(props) {
 
   return (
     <div className="display" id={"c_" + props.mode + "_display"}>
-      <a className="display-link" href={props.currentMedia.src} target="_blank">
+      <a className="display-link">
         {mediaTag}
       </a>
     </div>
@@ -389,7 +563,9 @@ function Display(props) {
 
 }
 
-//// Custom Hooks ////
+
+///|  Custom Hooks  |\\\
+
 function useGetS3Objects(bucketName) {
   const [objects, setObjects] = useState({
     list: [],
@@ -402,15 +578,43 @@ function useGetS3Objects(bucketName) {
     fetch("https://" + bucketName + ".s3.amazonaws.com/")
       .then((response) => response.text())
       .then((xml) => new window.DOMParser().parseFromString(xml, "text/xml"))
-      .then((data) => { setObjects({ list: data.querySelectorAll("Contents"), error: false }); console.log("DEBUG: API fetch @ " + Date.now()); })
+      .then((data) => { setObjects({ list: data.querySelectorAll("Contents"), error: false }); console.log("[DEBUG]: API fetch @ " + Date.now()); })
       .catch(error => setObjects({ list: [], error: error }));
   }, []);  // run once (never re-render)
 
   return objects;
 }
 
+// Event listener hook  [Recipe: https://usehooks.com/useEventListener/]
+function useEventListener(eventName, handler, element = window) {
+  // Create a ref that stores handler.
+  const savedHandler = useRef();
+  // Update ref.current value if handler changes.
+  // This allows our effect below to always get latest handler without us needing to
+  //  pass it in effect deps array and potentially cause effect to re-run every render.
+  useEffect(() => {
+    savedHandler.current = handler;
+  }, [handler]);
+  useEffect(
+    () => {
+      // Make sure element supports addEventListener.
+      const isSupported = element && element.addEventListener;
+      if (!isSupported) return;
+      // Create event listener that calls handler function stored in ref.
+      const eventListener = (event) => savedHandler.current(event);
+      // Add event listener
+      element.addEventListener(eventName, eventListener);
+      // Remove event listener on cleanup.
+      return () => {
+        element.removeEventListener(eventName, eventListener);
+      };
+    },
+    [eventName, element]  // Re-run if eventName or element changes.
+  );
+}
 
-//// Root Component ////
+
+///|  Root Component  |\\\
 function App() {
   return (
     <div className="App">
